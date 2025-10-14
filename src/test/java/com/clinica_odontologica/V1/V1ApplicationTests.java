4cffdc3895c3df0945fd7adf6ac3b8ba5bb1fe45
Oscar5718159 <benteison53@gmail.com>
2025-10-14 09:40:46 -0400

    Mi nuevo proyecto

---
+++ src/test/java/com/clinica_odontologica/V1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica_odontologica.V1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
