6e18cf08c9186e343c49ee2b29fd068dce584f04
Oscar <oscarmujica2006@gmail.com>
2025-11-05 11:50:12 -0400

    no tocar historial clinico falata completar xd

---
+++ src/main/java/com/clinica_odontologica/V1/Controller/DoctorViewController.java
@@ -0,0 +1,21 @@
+package com.clinica_odontologica.V1.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/doctor")
+public class DoctorViewController {
+
+    @GetMapping
+    public String paginaDoctor() {
+        return "doctor/doctor"; // ✅ Correcto
+    }   
+    
+    @GetMapping("/historial_clinico/historial_clinico_ENPROCESO")
+    public String mostrarDoctor() {
+        return "doctor/historial_clinico/historial_clinico_ENPROCESO"; 
+    }    
+    
+}
